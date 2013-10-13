61b648c82c9a5953a475fb04589b4f86a08af150
Craig Rose <megasaxon@gmail.com>
2013-10-13 10:28:29 +0200

    Added VCS module

--- puppet/modules/provision/manifests/last.pp
+++ puppet/modules/provision/manifests/last.pp
@@ -1,14 +1,13 @@
 class provision::last {
-
     exec { 'fetch composer':
         command => 'curl -sS https://getcomposer.org/installer | php',
         path    => ['/usr/bin', '/usr/sbin']
     }
 
-  exec { 'copy composer bin': 
-    command => 'mv composer.phar /usr/local/bin/composer',
-    require => Exec['fetch composer'],
-    unless  => 'test -f /usr/local/bin/composer',
-    path    => ['/bin', '/usr/bin', '/usr/sbin'],
-  }
+    exec { 'copy composer bin': 
+        command => 'mv composer.phar /usr/local/bin/composer',
+        require => Exec['fetch composer'],
+        unless  => 'test -f /usr/local/bin/composer',
+        path    => ['/bin', '/usr/bin', '/usr/sbin'],
+    }
 }
